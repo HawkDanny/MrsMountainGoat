24d869cb1a0d9c81996779829481e9cfab9a0482
Julia Del Matto <jdelmatto@comcast.net>
2020-03-01 20:13:04 -0500

    camera script

---
+++ Assets/movegoat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movegoat : MonoBehaviour
+{
+    private float speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+    }
+}
