3718b3834623f717b79b930d4b6ccb1c5a0fa239
Danny Hawk <hello@dannyhawk.com>
2020-03-01 20:31:00 -0500

    Made the goat prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,13 @@ public class GameManager : MonoBehaviour
     public string input;
     private int prevInput;
 
+    [Header("Goat")]
+    public Rigidbody2D[] legs;
+    private int currentLeg = 0;
+
+    [Header("Forces")]
+    public float stepForce;
+
     void Start()
     {
         input = "";
@@ -28,5 +35,20 @@ public class GameManager : MonoBehaviour
 
 
         prevInput = inputInt;
+
+
+
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Step();
+    }
+
+    private void Step()
+    {
+        legs[currentLeg].AddForce(new Vector2(0f, stepForce), ForceMode2D.Impulse);
+        currentLeg = (currentLeg + 1) % 4;
     }
 }
